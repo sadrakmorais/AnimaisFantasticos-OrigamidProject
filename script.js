10ebfc8e3383f32090f0a3997128b28fb1b4de7e
sadrakmorais <sadraksdk1@gmail.com>
2021-06-06 23:40:18 -0300

    Att

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
-// Retorne no console todas as imagens do site
+const tabmenu = document.querySelectorAll(".js-tabmenu li");
+const tabcontent = document.querySelectorAll(".js-tabcontent section");
+tabcontent[0].classList.add("ativo");
 
-const imagens = document.querySelectorAll('img')
-console.log(imagens)
-// Retorne no console apenas as imagens que começaram com a palavra imagem
-const nomap = document.querySelectorAll('[src^="img/imagem"]')
-console.log(nomap)
-// Selecione todos os links internos (onde o href começa com #)
-const links= document.querySelectorAll('[href^="#"]');
-console.log(links)
-// Selecione o primeiro h2 dentro de .animais-descricao
-const ph2 = document.querySelector('.animais-descricao h2')
-console.log(ph2)
-// Selecione o último p do site
-const paragrafos = document.querySelectorAll('p')
-console.log(paragrafos[paragrafos.length-1])
+if (tabcontent.length && tabmenu.length) {
+  function tabativa(index) {
+    tabcontent.forEach((e) => {
+      e.classList.remove("ativo");
+    });
+    tabcontent[index].classList.add("ativo");
+  }
 
-
+  tabmenu.forEach((e, index) => {
+    e.addEventListener("click", () => {
+      tabativa(index);
+    });
+  });
+}
